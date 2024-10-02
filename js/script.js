2058c1aa88a2ff2eb8c66eb0c002b8bbd6fce534
serna <114138913+sevendimensionalcrossproduct@users.noreply.github.com>
2024-10-02 03:12:00 -0700

    Finished fourth exercise

--- js/script.js
+++ js/script.js
@@ -19,13 +19,13 @@ else if (firstNumber === secondNumber){
 /*
 let language = prompt("Select language code: \nSpanish: es\nGerman: de\nEnglish: en\nFrench: fr");
 if (language === "es"){
-    document.write("<p>Hola mundo</p>");
+    console.log("Hola mundo");
 } else if (language === "de"){
-    document.write("<p>Hallo Welt</p>");
+    console.log("Hallo Welt");
 } else if (language === "fr"){
-    document.write("<p>Bonjour le monde</p>");
+    console.log("Bonjour le monde");
 } else {
-    document.write("<p>Hello world</p>");
+    console.log("Hello world");
 }
 */
 
@@ -35,20 +35,37 @@ let coinFlip = Math.round(Math.random());
 let choice = parseInt(prompt(`Select "Heads or Tails":\nEnter "0" for "Heads"\nEnter "1" for "Tails"`));
 
 if (choice !== 0 && choice !== 1){
-    document.write("Invalid input")
+    alert("Invalid input")
 } else {
     if(coinFlip === 0){
         if (coinFlip === choice){
-            document.write("<p>The flip was heads and you chose heads...you win!</p>");
+            alert("The flip was heads and you chose heads...you win!");
         } else if (coinFlip !== choice){
-            document.write("<p>The flip was heads but you chose tails...you lose!</p>");
+            alert("The flip was heads but you chose tails...you lose!");
         }
     } else if (coinFlip === 1) {
         if (coinFlip !== choice){
-            document.write("<p>The flip was tails but you chose heads...you lose!</p>");
+            alert("The flip was tails but you chose heads...you lose!");
         } else if (coinFlip === choice){
-            document.write("<p>The flip was tails and you chose tails...you win!</p>");
+            alert("The flip was tails and you chose tails...you win!");
         }
     }
 }
 */
+
+let grade = Number(prompt("Enter your grade:"));
+if (grade > 100 || grade < 1){
+    console.log("Invalid grade. Please enter a number in the correct range (1-100)");
+} else {
+    if (grade < 60) {
+        console.log("You received an F");
+    } else if (grade >= 60 && grade < 70) {
+        console.log("You received a D");
+    } else if (grade >= 70 && grade < 80){
+        console.log("You received a C");
+    } else if (grade >= 80 && grade < 90){
+        console.log("You received a B");
+    } else if (grade >= 90 && grade <= 100) {
+        console.log("You received an A");
+    }
+}
